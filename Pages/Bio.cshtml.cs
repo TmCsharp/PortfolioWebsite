428f5c8004775678ed08db79621386e71dd7e438
TmCsharp <HugonunAmk007>
2025-08-04 10:23:05 +0300

    Update+

--- Pages/Bio.cshtml.cs
+++ Pages/Bio.cshtml.cs
@@ -1,42 +1,12 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Collections.Generic;
 
 namespace PortfolioWebsite.Pages
 {
     public class BioModel : PageModel
     {
-        public string FullName { get; set; } = "Your Name";
-        public string ProfessionalTitle { get; set; } = "Your Profession";
-        public List<string> Skills { get; set; } = new List<string>();
-        public List<Education> EducationHistory { get; set; } = new List<Education>();
-
-        public class WorkExperience
-        {
-            public string Title { get; set; }
-            public string Company { get; set; }
-            public string Duration { get; set; }
-            public List<string> Responsibilities { get; set; }
-        }
-
-        public class Education
-        {
-            public string Degree { get; set; }
-            public string Institution { get; set; }
-            public string Year { get; set; }
-        }
-
         public void OnGet()
         {
-            Skills = new List<string> { "C#", "ASP.NET Core", "SQL" };
-            EducationHistory = new List<Education>
-            {
-                new Education
-                {
-                    Degree = "BSc Computer Science",
-                    Institution = "State University",
-                    Year = "2023"
-                }
-            };
+
         }
     }
 }
--- Pages/Projects.cshtml.cs
+++ Pages/Projects.cshtml.cs
@@ -1,12 +1,71 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PortfolioWebsite.Pages
 {
     public class ProjectsModel : PageModel
     {
+        public List<GitHubProject> Projects { get; set; } = new();
+
         public void OnGet()
         {
+            Projects = new List<GitHubProject>
+            {
+                new GitHubProject(
+                    "Portfolio Website",
+                    "This responsive portfolio built with ASP.NET Core and Razor Pages",
+                    new List<string> { "C#", "Razor", "CSS", "Html" },
+                    "https://github.com/TmCsharp/PortfolioWebsite"
+                ),
+
+                new GitHubProject(
+                    "Entity Framework Core & MSSQL Database",
+                    "A collection of practical exercises implementing Entity Framework Core with MSSQL.",
+                    new List<string> { ".NET 8", "EF Core", "MSSQL"},
+                    "https://github.com/TmCsharp/DB-MSSQL_EntityFrameworkCore"
+                ),
+
+                new GitHubProject(
+                    "Advanced & OOP",
+                    "A collection of practical exercises implementing Object Oriented Programming.",
+                    new List<string> { ".NET 8", "C#", "OOP" },
+                    "https://github.com/TmCsharp/Advanced_OOP"
+                ),
+
+                  new GitHubProject(
+                    "Fundamentals",
+                    "A collection of practical exercises covering core programming concepts",
+                    new List<string> { ".NET 8", "C#", "Algorithms" },
+                    "https://github.com/TmCsharp/Fundamentals"
+                ),
+
+                  new GitHubProject(
+                    "Hangman Console Game",
+                    "A classic word-guessing game built with C#, featuring ASCII art, customizable word lists",
+                    new List<string> { ".NET 8", "C#", "ASCII Art" },
+                    "https://github.com/TmCsharp/Fundamentals"
+                ),
+
+                  new GitHubProject(
+                    "Rock/Paper/Scissors Console Game",
+                    "A classic hand game implemented in C# with ASCII art visuals and score tracking.",
+                    new List<string> { ".NET 8", "C#", "Game Logic" },
+                    "https://github.com/TmCsharp/RockPaperScissors"
+                ),
+
+                  new GitHubProject(
+                    "Guess the Number Game",
+                    "Console-based classic guess-the-number challenge in C#.",
+                    new List<string> { ".NET 8", "C#", "Game Logic" },
+                    "https://github.com/TmCsharp/GuessANumber"
+                )
+            };
         }
     }
+
+    public record GitHubProject(
+        string Title,
+        string Description,
+        List<string> Technologies,
+        string Url
+    );
 }
